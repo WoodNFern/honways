e86633a22127bc3250600e3f201ca6089d366110
Matthias Hermann <matthias.h@online.de>
2019-08-18 11:16:54 +0200

    Create a dummy, line-based processing pipeline

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,15 @@ module Main where
 
 import System.IO
 
+processLines :: [String] -> [String]
+processLines (x:xs) = (processLine x) : (processLines xs)
+processLines []     = []
+
+processLine :: String -> String
+processLine x = "Processed one line\n"
+
 main :: IO ()
 main = do
    contents <- readFile "input.txt"
-   putStr contents
+   putStr . foldr (++) "" $ processLines $ lines contents
    writeFile "output.txt" contents
