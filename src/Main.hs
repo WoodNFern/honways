a1fcebda63e1d3e98ba97ffcdc87f2681b2d5ec0
Matthias Hermann <matthias.h@online.de>
2019-08-20 12:02:39 +0200

    Add function to replace single chars for displays

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,11 @@ processLines []     = []
 processLine :: String -> String
 processLine x = "Processed one line\n"
 
+replaceChar :: Char -> Char
+replaceChar '0' = ' '
+replaceChar '1' = '█'
+replaceChar x = x
+
 main :: IO ()
 main = do
    contents <- readFile "input.txt"
