acb283cbdc68252e38a7c1216628096e7a1561bc
Matthias Hermann <matthias.h@online.de>
2019-08-21 16:50:57 +0200

    Add function to sum neighbors of element

--- src/Main.hs
+++ src/Main.hs
@@ -22,6 +22,12 @@ textifyElement x
     | x <= 0.5  = ' '
     | x > 0.5   = '█'
 
+sumOfNeighbors :: (Num a, Show a) => (Int, Int) -> Matrix a -> a
+sumOfNeighbors (x, y) m = foldr (+) 0 $ getNeighborElements (x, y) m
+
+getNeighborElements :: (Int, Int) -> Matrix a -> a
+getNeighborElements (x, y) m = map (\(r, c) -> getElem r c m) $ getNeighborIndices (x, y) m
+
 getNeighborIndices :: (Int, Int) -> Matrix a -> [(Int, Int)]
 getNeighborIndices (x, y) m = [ (r, c) | r <- [1 .. nrows m], c <- [1 .. ncols m],
                                          (abs (x - r)) <= 1, (abs (y - c)) <= 1,       -- all elements in radius 1
